55ef4367ef41e39ca2e1a59b4471abe4f5cf6ee5
Martin Artola <martin.artola@swisslife.ch>
2024-12-16 13:59:47 +0100

    style: use biome

--- src/components/profile-button.tsx
+++ src/components/profile-button.tsx
@@ -1,37 +1,37 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
-import { useProfile } from "@/hooks/use-profile";
+import {Button} from '@/components/ui/button';
+import {useProfile} from '@/hooks/use-profile';
+import Image from 'next/image';
+import Link from 'next/link';
 
 interface ProfileButtonProps {
   profileId: string;
   link?: string;
-  variant?: "link" | "secondary";
+  variant?: 'link' | 'secondary';
 }
 
 export const ProfileButton = ({
   profileId,
   link,
-  variant = "link",
+  variant = 'link',
 }: ProfileButtonProps) => {
-  const { profile } = useProfile(profileId);
+  const {profile} = useProfile(profileId);
 
   if (!profile) return null;
 
-  console.log({ profile });
+  console.log({profile});
 
   const ButtonContent = (
     <Button
       variant={variant}
       className={`flex items-center gap-2 ${
-        link ? "" : "cursor-default pointer-events-none"
-      } ${variant === "link" ? "px-0" : ""}`}
+        link ? '' : 'cursor-default pointer-events-none'
+      } ${variant === 'link' ? 'px-0' : ''}`}
     >
       <div
         className={`w-7 h-7 flex items-center justify-center ${
-          variant === "link" ? "bg-primary/10 rounded-full p-1" : ""
+          variant === 'link' ? 'bg-primary/10 rounded-full p-1' : ''
         }`}
       >
         <Image
